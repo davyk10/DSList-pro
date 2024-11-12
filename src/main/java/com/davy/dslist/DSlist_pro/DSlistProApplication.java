a236b2cec003b81965cd22f754742eb2255281eb
Davy Kauã <morningstar5657888@gmail.com>
2024-11-11 22:32:04 -0300

    project created

---
+++ src/main/java/com/davy/dslist/DSlist_pro/DSlistProApplication.java
@@ -0,0 +1,12 @@
+package com.davy.dslist.DSlist_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSlistProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSlistProApplication.class, args);
+	}
+}
---
+++ src/test/java/com/davy/dslist/DSlist_pro/DSlistProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davy.dslist.DSlist_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSlistProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
